65dc3d86d0b5f529842cb55fbdf6659bb4a4ba08
alex-artamonov <alex.artamonov2010@gmail.com>
2022-06-09 13:36:08 +0300

    change raise to bot.reply in andle_to(message, from_: str):

--- currency_bot.py
+++ currency_bot.py
@@ -110,8 +110,8 @@ def handle_to(message, from_: str):
     try:
         to = currencies_complete[message.text.lower()]
     except KeyError as e:
-        raise KeyError(f"Валюта <b>\"{message.text}\"</b> в базе не обнаружена."
-                       f"\nПопробуйте еще раз, набрав или нажав /convert.")
+        bot.reply_to(message.chat.id, f"Валюта <b>\"{message.text}\"</b> в базе не обнаружена."
+                       f"\nПопробуйте еще раз, набрав или нажав /convert.", parse_mode="HTML")
     else:
         bot.register_next_step_handler(message, handle_amount, from_, to)
         bot.send_message(message.chat.id, "Введите количество:")
